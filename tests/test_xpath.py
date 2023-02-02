7cdd03e8a8196d4edce365010b3e9b680e644487
john balo <john-blmns@protonmail.com>
2023-02-02 21:41:13 +0000

    push forgotten xpath test

---
+++ tests/test_xpath.py
@@ -0,0 +1,39 @@
+from bs4 import BeautifulSoup
+from unittest import TestCase
+
+from src.utils.xpath import element_xpath
+
+
+class TestXpath(TestCase):
+    def test_element_xpath(self):
+        cases = [
+            {"html": "<html id=\"this\"></html>", "xpath": "/html"},
+            {"html": "<html id=\"this\"><body><p></p></body></html>", "xpath": "/html"},
+            {"html": (
+                "<html><body>"
+                "<p id=\"this\"></p>"
+                "</body></html>"
+            ), "xpath": "/html/body/p"},
+            {"html": (
+                "<html><body>"
+                "<p></p>"
+                "<p></p>"
+                "<p id=\"this\"></p>"
+                "</body></html>"
+            ), "xpath": "/html/body/p[3]"},
+            {"html": (
+                "<html><body>"
+                "<p></p>"
+                "<p id=\"this\"></p>"
+                "<p></p>"
+                "</body></html>"
+            ), "xpath": "/html/body/p[2]"},
+        ]
+
+        for case in cases:
+            soup = BeautifulSoup(case["html"], "html.parser")
+            tag = soup.find(attrs={"id": "this"})
+            self.assertEqual(element_xpath(tag), case["xpath"])
+
+    def test_element_xpath_error(self):
+        self.assertEqual(element_xpath("no tag"), "unable to calculate xpath")
